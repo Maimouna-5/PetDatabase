1bb5e8fb5bc3de22e4488c8ac76779b871223dba
Maimouna-5 <93747248+Maimouna-5@users.noreply.github.com>
2021-11-12 21:38:36 -0600

    Update: Allow read/write from file (#1)
    Co-authored-by: Maimouna Diallo <maimounadiallo@Maimounas-MacBook-Pro.local>

--- src/main/java/com/petdatabse/petdatabase/Pet.java
+++ src/main/java/com/petdatabse/petdatabase/Pet.java
@@ -1,53 +1,53 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 package com.petdatabse.petdatabase;
 
-/**
- *
- * @author maimounadiallo
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
  */
+
 class Pet {
-    private String name;
-    private int age;
-    private boolean isRemoved;
+	private String name;
+	private int age;
+	private boolean isRemoved;
 
-    //Constructor
-    public Pet(String name, int age) {
-            this.name = name;
-            this.age = age;
-            isRemoved = false;
-    }
+	//Constructor
+	public Pet(String name, int age) {
+		this.name = name;
+		this.age = age;
+		isRemoved = false;
+	}
 
-    public Pet(){
-            isRemoved = true;
-    }
+	public Pet(){
+		isRemoved = true;
+	}
 
-    //Setters
-    void setName(String name){
-            this.name = name;
-    }
+	//Setters
+	void setName(String name){
+		this.name = name;
+	}
 
-    void setAge(int age){
-            this.age = age;
-    }
+	void setAge(int age){
+		this.age = age;
+	}
 
-    void setIsRemoved(boolean isRemoved){
-            this.isRemoved = isRemoved;
-    }
+	void setIsRemoved(boolean isRemoved){
+		this.isRemoved = isRemoved;
+	}
 
-    //Getters
-    String getName(){
-            return name;
-    }
+	//Getters
+	String getName(){
+		return name;
+	}
 
-    int getAge(){
-            return age;
-    }
+	int getAge(){
+		return age;
+	}
 
-    boolean getIsRemoved(){
-            return isRemoved;
-    }
+	boolean getIsRemoved(){
+		return isRemoved;
+	}
+        
+        @Override public String toString() {
+		return name + " " + age;
+	}
 }
